4701bef0b0be3daa0b3deb91f1ca198d1ff1698d
Andre Roelofs <p.arroelofs@gmail.com>
2020-12-24 05:13:25 +0100

    added some things

--- conv_autoencoder.py
+++ conv_autoencoder.py
@@ -12,7 +12,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn import preprocessing
 from torchvision import transforms
 from torchvision.utils import save_image
-from torchvision.transforms.functional import resize
+from torchvision.transforms.functional import resize, center_crop, gaussian_blur
 from torchvision.datasets import MNIST, CIFAR10
 import matplotlib.pyplot as plt
 import os
@@ -31,23 +31,30 @@ one_shot_classifier = False
 if one_shot_classifier:
     train_batch_size = 1
 epochs = 1
-use_relu = False
+use_relu = True
 filters = 1
-size = 7
+size = 14
 RBM_VISIBLE_UNITS = filters * size ** 2
-MIN_FAMILIARITY_THRESHOLD = 1
+MIN_FAMILIARITY_THRESHOLD = 10
 variance = 0.07
-RBM_HIDDEN_UNITS = 5
+RBM_HIDDEN_UNITS = 1
 torch.manual_seed(0)
 np.random.seed(0)
 
 # %% Load data
-train_data = MNIST('./data', train=False, download=True,
+train_data = MNIST('./data', train=True, download=True,
                      transform=transforms.Compose([
-                         transforms.ToTensor()]))
+                         transforms.ToTensor(),
+                         transforms.CenterCrop(size),
+                     ]))
+
+# train_data.data = train_data.data[:10000]
+# train_data.targets = train_data.targets[:10000]
 
 test_data = MNIST('./data', train=False, transform=transforms.Compose([
-    transforms.ToTensor()]))
+    transforms.ToTensor(),
+    transforms.CenterCrop(size),
+]))
 
 
 # %% Define encoder
@@ -57,16 +64,19 @@ class Encoder(nn.Module):
 
         self.conv1 = nn.Conv2d(1, filters, (3, 3), stride=1, padding=1)
 
-        nn.init.normal_(self.conv1.weight, 5.0, 1.0)
+        # nn.init.normal_(self.conv1.weight, 5.0, 1.0)
         # nn.init.normal_(self.conv1.weight, 0, 0.0007)
         # nn.init.xavier_normal_(self.conv1.weight, 0.007)
         # nn.init.xavier_normal_(self.conv1.weight, 5.0)
-        # nn.init.xavier_normal_(self.conv1.weight, 0.07)
+        nn.init.xavier_normal_(self.conv1.weight, 1.0)
 
         if use_relu:
             self.act = nn.ReLU()
         else:
             self.act = nn.SELU()
+
+        for p in self.parameters():
+            p.data.clamp_(0)
 
     def forward(self, x):
         x = self.act(self.conv1(x))
@@ -91,34 +101,23 @@ class Network(nn.Module):
 
 
 # %%
-class SVM(nn.Module):
-
-    def __init__(self):
-        super().__init__()  # Call the init function of nn.Module
-        self.fully_connected = nn.Linear(2, 1)  # Implement the Linear function
-
-    def forward(self, x):
-        fwd = self.fully_connected(x)  # Forward pass
-        return fwd
-
-#%%
 class Classifier(nn.Module):
     def __init__(self, n_features):
         super().__init__()
         self.device = torch.device("cuda")
 
-        # self.fc1 = nn.Linear(n_features, 100)
-        # self.fc2 = nn.Linear(200, 200)
-        # self.fc3 = nn.Linear(200, 100)
-        self.fc4 = nn.Linear(n_features, 10)
+        self.fc1 = nn.Linear(n_features, 400)
+        self.fc2 = nn.Linear(400, 200)
+        self.fc3 = nn.Linear(200, 100)
+        self.fc4 = nn.Linear(100, 10)
 
-        # self.fc1_bn = nn.BatchNorm1d(100)
-        # self.fc2_bn = nn.BatchNorm1d(200)
-        # self.fc3_bn = nn.BatchNorm1d(100)
+        self.fc1_bn = nn.BatchNorm1d(400)
+        self.fc2_bn = nn.BatchNorm1d(200)
+        self.fc3_bn = nn.BatchNorm1d(100)
         self.fc4_bn = nn.BatchNorm1d(10)
 
-        # self.act = nn.SELU()
-        self.act = nn.ReLU()
+        self.act = nn.SELU()
+        # self.act = nn.ReLU()
 
         # if use_relu:
         #     self.act = nn.ReLU()
@@ -128,12 +127,12 @@ class Classifier(nn.Module):
         self.to(self.device)
 
     def forward(self, x):
-        # x = self.fc1_bn(self.fc1(x))
-        # x = self.act(x)
-        # x = self.fc2_bn(self.fc2(x))
-        # x = self.act(x)
-        # x = self.fc3_bn(self.fc3(x))
-        # x = self.act(x)
+        x = self.fc1_bn(self.fc1(x))
+        x = self.act(x)
+        x = self.fc2_bn(self.fc2(x))
+        x = self.act(x)
+        x = self.fc3_bn(self.fc3(x))
+        x = self.act(x)
         x = self.fc4(x)
         x = self.fc4_bn(x)
 
@@ -218,7 +217,8 @@ class WDN(nn.Module):
                 flat_rbm_input = rbm_input.view(len(rbm_input), RBM_VISIBLE_UNITS)
 
                 # Compare data with existing models
-                if m.rbm.is_familiar(flat_rbm_input, provide_value=False):
+                familiarity = m.rbm.is_familiar(flat_rbm_input, provide_value=False)
+                if familiarity > train_batch_size / 2:
                     n_familiar += 1
                 if n_familiar >= MIN_FAMILIARITY_THRESHOLD or n_familiar + (
                         a_n_models - model_counter) < MIN_FAMILIARITY_THRESHOLD:
@@ -235,13 +235,17 @@ class WDN(nn.Module):
 
             self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
 
-            for i in range(5):
+            for i in range(50):
                 # Encode the image
                 rbm_input = self.model.encode(data)
                 # Resize and flatten input for RBM
                 rbm_input = resize(rbm_input, [size, size])
                 flat_rbm_input = rbm_input.view(len(rbm_input), RBM_VISIBLE_UNITS)
 
+                familiarity = self.model.rbm.is_familiar(flat_rbm_input, provide_value=False)
+                if familiarity == train_batch_size:
+                    self.model.rbm.calculate_energy_threshold(flat_rbm_input)
+                    break
                 # Train RBM
                 rbm_error = self.model.rbm.contrastive_divergence(flat_rbm_input, update_weights=True)
 
@@ -249,19 +253,7 @@ class WDN(nn.Module):
                 visible = self.model.rbm.sample_visible(hidden).reshape((data.shape[0], filters, size, size))
                 loss = self.loss_function(visible, rbm_input)
                 loss.backward(retain_graph=True)
-
-            # # Sample RBM
-            #
-            #
-            # # Train Encoder
-            # for i in range(2):
-            #     rbm_input = self.model.encode(data)
-            #     # Resize and flatten input for RBM
-            #     rbm_input = resize(rbm_input, [size, size])
-            #     flat_rbm_input = rbm_input.view(len(rbm_input), RBM_VISIBLE_UNITS)
-
-
-            self.model.rbm.calculate_energy_threshold(flat_rbm_input)
+                self.model.rbm.calculate_energy_threshold(flat_rbm_input)
 
 
 # %% Instantiate the model
@@ -269,7 +261,7 @@ class WDN(nn.Module):
 model = WDN()
 
 # %% Train the model
-for i in range(20):
+for i in range(10):
     print("Training digit: ", i)
     subset_indices = (torch.tensor(train_data.targets) == i).nonzero().view(-1)
     for epoch in range(epochs):
@@ -292,7 +284,6 @@ def predict_classifier():
         target = target.to(clf.device)
         out = clf(data)
         test_loss += clf.loss_function(out, target).item()
-        # test_loss += clf.loss_function(out, target.long()).item()
         pred = out.data.max(1)[1]
         target_pred = target.data.max(1)[1]
         correct += pred.eq(target_pred).sum()
@@ -309,12 +300,9 @@ def train_classifier():
         for batch_idx, (data, target) in enumerate(train_dataset_loader):
             data = data.to(clf.device)
             target = target.to(clf.device)
-
             optimizer.zero_grad()
             out = clf(data)
-
             loss = clf.loss_function(out, target)
-            # loss = clf.loss_function(out, target.long())
             loss.backward()
             optimizer.step()
 
@@ -327,7 +315,7 @@ def train_classifier():
 
 # %% Convert the training set to the unsupervised latent vector
 print("Converting images to latent vectors")
-classifier_training_batch_size = 100
+classifier_training_batch_size = 1000
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=classifier_training_batch_size, shuffle=False)
 counter = 0
 training_features = []
@@ -359,7 +347,7 @@ for batch_idx, (data, target) in enumerate(train_loader):
         test_target = np.zeros(10, dtype=float)
         test_target[target_labels[i]] = 1.0
         # training_features.append(latent_vector[:, i])
-        training_features.append(latent_vector)
+        training_features.append(latent_vector[:, i])
         # training_labels.append(target_labels[i])
         training_labels.append(test_target)
 
@@ -370,11 +358,13 @@ for batch_idx, (data, target) in enumerate(train_loader):
 training_features = np.array(training_features)
 training_features_norm = preprocessing.scale(training_features)
 training_labels = np.array(training_labels, dtype=float)
+#%%
 train_dataset = UnsupervisedVectorDataset(training_features_norm, training_labels)
+train_dataset_loader = torch.utils.data.DataLoader(train_dataset, batch_size=100, shuffle=True)
 
 # %%
 print("Converting test images to latent vectors")
-test_batch_size = 1
+test_batch_size = 1000
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=test_batch_size, shuffle=False)
 counter = 0
 test_features = []
@@ -407,8 +397,7 @@ for batch_idx, (data, target) in enumerate(test_loader):
     for i in range(test_batch_size):
         test_target = np.zeros(10, dtype=float)
         test_target[target_labels[i]] = 1
-        # test_features.append(latent_vector[:, i])
-        test_features.append(latent_vector)
+        test_features.append(latent_vector[:, i])
         # test_labels.append(target_labels[i])
         test_labels.append(test_target)
 
@@ -425,7 +414,7 @@ for batch_idx, (data, target) in enumerate(test_loader):
 test_features = np.array(test_features)
 test_features_norm = preprocessing.scale(test_features)
 test_labels = np.array(test_labels)
-
+#%%
 test_dataset = UnsupervisedVectorDataset(test_features_norm, test_labels)
 test_dataset_loader = torch.utils.data.DataLoader(test_dataset, batch_size=100, shuffle=False)
 
@@ -434,9 +423,6 @@ print("Training classifier")
 clf = Classifier(training_features_norm.shape[1])
 # criterion = nn.NLLLoss()
 # criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(clf.parameters(), lr=1e-3, amsgrad=False)
-train_dataset_loader = torch.utils.data.DataLoader(train_dataset, batch_size=100, shuffle=True)
+optimizer = torch.optim.Adam(clf.parameters(), lr=1e-3, amsgrad=True)
 
 train_classifier()
-
-
